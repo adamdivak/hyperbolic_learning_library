c65cb096a3e27245a34ed39f5a70915f1c55957f
Adam Divak <adam.divak@gmail.com>
2025-05-13 20:58:32 +0200

    Add optional scaling to LayerNorm

--- hypll/nn/modules/layernorm.py
+++ hypll/nn/modules/layernorm.py
@@ -1,4 +1,5 @@
 from torch.nn import Module, LayerNorm
+import math
 
 from hypll.manifolds import Manifold
 from hypll.tensors import ManifoldTensor
@@ -10,22 +11,25 @@ from torch.nn.modules.normalization import _shape_t
 
 class HLayerNorm(Module):
     """ LayerNorm - inefficient implementation in tangent space.
-    Signature identical to PyTorch's LayerNorm, except for the additional manifold parameter """
+    Signature identical to PyTorch's LayerNorm, except for the additional manifold parameter, plus a division by sqrt(dim) """
     def __init__(self, normalized_shape: _shape_t,
                 manifold: Manifold,
                 eps: float = 1e-5,
                 elementwise_affine: bool = True,
                 bias: bool = True,
+                scale_by_d: bool = False,
                 device=None,
                 dtype=None,
         ) -> None:
         
         super(HLayerNorm, self).__init__()
+        self.normalized_shape = normalized_shape
         self.manifold = manifold
         self.euclidean_ln = LayerNorm(normalized_shape, 
             eps=eps,
             elementwise_affine=elementwise_affine,
             bias=bias,
+            scale_by_d=scale_by_d,
             device=device,
             dtype=dtype,
         )
@@ -33,7 +37,10 @@ class HLayerNorm(Module):
     def forward(self, input: ManifoldTensor) -> ManifoldTensor:
         check_if_manifolds_match(layer=self, input=input)
         return op_in_tangent_space(
-            op=self.euclidean_ln,
+            # Original LayerNorm calculation divided by sqrt(dim), in order to keep the norm
+            # of the points identical.
+            # FIXME: math.sqrt(self.normalized_shape) currently only works for int shapes, handle multidim shapes here
+            op=lambda x: self.euclidean_ln(x) / math.sqrt(self.normalized_shape) if not self.scale_by_d else self.euclidean_ln(x),
             manifold=self.manifold,
             input=input,
         )
